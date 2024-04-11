83e07a410dc0d4a17cc43fa48b3747b5064e3068
Liam <liamisrael200@gmail.com>
2024-04-11 00:19:45 -0400

    Added refresh updated html

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,19 @@
 use actix_web::{web, App, HttpServer};
 mod data;
 mod views;
+use std::thread;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    thread::spawn(|| {views::reload()});
+
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(views::index))
             .route("/tracker", web::get().to(views::tracker))
+            .route("/tracker/node", web::get().to(views::node))
+            .route("/tracker/nodes", web::get().to(views::nodes))
+
             .route("/credits", web::get().to(views::credits))
 
             .route("/data/", web::get().to(data::fetch_device_names))
--- src/views.rs
+++ src/views.rs
@@ -1,41 +1,82 @@
 use actix_web::{Responder, HttpResponse};
 use tera::{Tera, Context};
 use lazy_static::lazy_static;
-
+use std::sync::Mutex;
+use std::io;
+use std::process;
 
 lazy_static! {
-    pub static ref TEMPLATES: Tera = {
-        let tera = match Tera::new("templates/**/*") {
+    pub static ref TEMPLATES: Mutex<Tera> = {
+        let mut tera = match Tera::new("templates/**/*") {
             Ok(t) => t,
             Err(e) => {
                 println!("Parsing error(s): {}", e);
                 ::std::process::exit(1);
             }
         };
-        tera
+        
+        Mutex::new(tera)
     };
 }
 
 pub async fn index() -> impl Responder {
     let context = Context::new();
-    let string = TEMPLATES.render("index.html", &context).unwrap();
+    let string = TEMPLATES.lock().unwrap().render("index.html", &context).unwrap();
     HttpResponse::Ok()
-    .content_type("text/html")
-    .body(string)
+        .content_type("text/html")
+        .body(string)
 }
 
 pub async fn tracker() -> impl Responder {
     let context = Context::new();
-    let string = TEMPLATES.render("tracker.html", &context).unwrap();
-    HttpResponse::Ok()
-    .content_type("text/html")
-    .body(string)
-}
-
-pub async fn credits() -> impl Responder{
-    let context = Context::new();
-    let string = TEMPLATES.render("credits.html", &context).unwrap();
+    let string = TEMPLATES.lock().unwrap().render("tracker.html", &context).unwrap();
     HttpResponse::Ok()
         .content_type("text/html")
         .body(string)
+}
+
+pub async fn node() -> impl Responder {
+    let context = Context::new();
+    let string = TEMPLATES.lock().unwrap().render("node.html", &context).unwrap();
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(string)
+}
+
+pub async fn nodes() -> impl Responder {
+    let context = Context::new();
+    let string = TEMPLATES.lock().unwrap().render("nodes.html", &context).unwrap();
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(string)
+}
+
+pub async fn credits() -> impl Responder {
+    let context = Context::new();
+    let string = TEMPLATES.lock().unwrap().render("credits.html", &context).unwrap();
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(string)
+}
+
+pub fn reload() {
+    println!("To Exit Enter QUIT:");
+    loop {
+        println!("Press Enter To Reload Templates:");
+        // Create a mutable string to store user input
+        let mut input = String::new();
+
+        // Read user input from the command line
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {    
+                let input = input.trim().to_ascii_uppercase();
+                if input == "QUIT" {
+                    process::exit(0);
+                } else {
+                    TEMPLATES.lock().unwrap().full_reload();
+                }
+            }
+            Err(error) => println!("Error reading input: {}", error),
+        }
+    }
 }
